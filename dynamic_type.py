6aa23fedb8cf63044c9ed8497cee0d367622fe8e
yashpansuriya-dev <pansuriyayashtechforce@gmail.com>
2026-02-26 16:21:46 +0530

    new file

---
+++ dynamic_type.py
@@ -0,0 +1,21 @@
+a =5
+print(type(a))
+
+a="yash pansuriya"
+print(type(a))
+
+a=8.5
+print(type(a))
+
+b=None
+print(type(b))
+
+a=True
+print(type(a))
+
+name="Yash Pansuriya"
+cgpa = 8.6
+passing = True
+marks = 81
+
+print(f"hi , my name is {name} , and i am doing my final year from vgec college and i got {marks} in last semester , so my overall cgpa is {cgpa} , so i am {"passed" if passing else "failed"} ")
